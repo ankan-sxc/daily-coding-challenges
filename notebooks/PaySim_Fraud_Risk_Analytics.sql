18aaac64c0c612afa9b5cd48ff8a080f6a7b3013
ANKAN M <maitiankan5@gmail.com>
2026-09-02 23:16:56 +0530

    Day-2(02/09/2026):Level-1 | PaySim

--- notebooks/PaySim_Fraud_Risk_Analytics.sql
+++ notebooks/PaySim_Fraud_Risk_Analytics.sql
@@ -1,15 +1,28 @@
-CREATE TABLE paysim (
-    step INTEGER,
-    type VARCHAR(20),
-    amount NUMERIC(18,2),
-    nameOrig VARCHAR(50),
-    oldbalanceOrg NUMERIC(18,2),
-    newbalanceOrig NUMERIC(18,2),
-    nameDest VARCHAR(50),
-    oldbalanceDest NUMERIC(18,2),
-    newbalanceDest NUMERIC(18,2),
-    isFraud SMALLINT,
-    isFlaggedFraud SMALLINT
-);
-
+--Day 2:
+----Level-1:Beginner
+--1.Count total transactions
 SELECT COUNT(*) FROM paysim;
+--2.Calculate total transaction amount
+select sum(amount) from paysim as total_transaction_amount;
+--3.Average transaction amount across all PaySim transactions.
+select avg(amount) as average_transaction_amount from paysim;
+--4.The number of transactions for each transaction type.
+select type,
+       count(*) as transaction_count 
+from paysim 
+group by type;
+--5.Count fraudulent transactions
+select type,
+       count(*) as fraud_transaction_count
+	   from paysim
+where isFraud=1
+group by type;
+--6.What percentage of transactions within each transaction type are confirmed fraudulent?
+SELECT
+    type,
+    COUNT(*) AS transaction_count,
+    SUM(CASE WHEN isFraud = 1 THEN 1 ELSE 0 END) AS fraud_transaction_count,
+    100.0 * SUM(CASE WHEN isFraud = 1 THEN 1 ELSE 0 END) / COUNT(*) AS fraud_rate
+FROM paysim
+GROUP BY type;
+--7.
